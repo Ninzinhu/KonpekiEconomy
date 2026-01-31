7a4ab9d340d1255ec43c11e5b9b80392609b511b
Joao Vasconcelos <pel118201@gmail.com>
2026-01-31 17:46:20 -0300

    Add /wallet command with placeholder GUI implementation

---
+++ src/main/java/org/konpekiestudios/konpekistudios/hytale/plugins/commands/WalletCommand.java
@@ -0,0 +1,49 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package org.konpekiestudios.konpekistudios.hytale.plugins.commands;
+
+import org.konpekiestudios.konpekistudios.hytale.plugins.KonpekiEconomy;
+import org.konpekiestudios.konpekistudios.hytale.plugins.config.ConfigManager;
+import com.hypixel.hytale.protocol.GameMode;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+
+import javax.annotation.Nonnull;
+
+public class WalletCommand extends CommandBase {
+    private final ConfigManager configManager;
+
+    public WalletCommand(@Nonnull String name, @Nonnull String description) {
+        super(name, description);
+        this.addAliases(new String[]{"carteira"});
+        this.setPermissionGroup(GameMode.Adventure);
+        this.configManager = KonpekiEconomy.getInstance().getConfigManager();
+    }
+
+    protected void executeSync(@Nonnull CommandContext commandContext) {
+        if (commandContext.sender() instanceof Player) {
+            Player player = (Player) commandContext.sender();
+            try {
+                // Open wallet GUI
+                openWalletGUI(player);
+            } catch (Exception e) {
+                commandContext.sender().sendMessage(Message.raw(this.configManager.getData().messages.error_occurred));
+                throw new RuntimeException(e);
+            }
+        } else {
+            commandContext.sender().sendMessage(Message.raw("This command can only be executed by a player."));
+        }
+    }
+
+    private void openWalletGUI(Player player) {
+        // TODO: Implement GUI - Hytale API may not have inventory GUIs like Minecraft
+        // For now, send balance message
+        double balance = KonpekiEconomy.getInstance().getEconomy().getBalance(player.getPlayerRef().getUuid());
+        player.sendMessage(Message.raw("Carteira: Saldo " + balance + " " + this.configManager.getData().chat.currency));
+    }
+}
